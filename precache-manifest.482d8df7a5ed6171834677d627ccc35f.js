316140ed458496da1f06f10e58fac4ee995572f8
Mikko Maja <mikmaj88@gmail.com>
2019-01-15 14:16:21 +0200

    Updates

---
+++ precache-manifest.482d8df7a5ed6171834677d627ccc35f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d6ba1503e8c2033350d",
+    "url": "/fcc-react-calculator/static/js/runtime~main.2d6ba150.js"
+  },
+  {
+    "revision": "5e49a0473a8fca992128",
+    "url": "/fcc-react-calculator/static/js/main.5e49a047.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/fcc-react-calculator/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5e49a0473a8fca992128",
+    "url": "/fcc-react-calculator/static/css/main.a645381d.chunk.css"
+  },
+  {
+    "revision": "912ac2725ae9d968a0710a93c6d8966b",
+    "url": "/fcc-react-calculator/index.html"
+  }
+];
